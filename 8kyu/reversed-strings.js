ec14dee71bad6347170fcc306545e5581cb6376b
safronman <safronmanbox@gmail.com>
2023-06-26 16:51:54 +0300

    reversed-strings.js - 3

--- 8kyu/reversed-strings.js
+++ 8kyu/reversed-strings.js
@@ -1,11 +1,11 @@
-// DESCRIPTION:
+// ❓ DESCRIPTION:
 //     Complete the solution so that it reverses the string passed into it.
 //
 // 'world'  =>  'dlrow'
 // 'word'   =>  'drow'
 
 
-// SOLUTION:
+// ✅ SOLUTION:
 function solution(str){
     return str.split('').reverse().join('');
 }
